eac19ccd182c593387c913a6e71aa25864414228
Daniel <40139515+yiluun@users.noreply.github.com>
2022-12-11 00:55:28 -0800

    Delete main.py

--- website/main.py
+++
@@ -1,9 +0,0 @@
-from website import create_app
-
-# to add more html https://youtu.be/dam0GPOAvVI?t=1756
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True) # automatically reruns web server when code changes. turn off in production
-
